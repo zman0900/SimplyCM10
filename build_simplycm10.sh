62e5a140b986eb28aa7459341fc66652fa98ec72
Dan Ziemba <zman0900@gmail.com>
2012-09-27 07:14:21 -0400

    Initial commit

---
+++ build_simplycm10.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ "$CPU_JOB_NUM" = "" ] ; then
+	CPU_JOB_NUM=`grep -c processor /proc/cpuinfo`
+fi
+
+CUSTOMVERSION="SimplyCM10-v1"
+export KBUILD_BUILD_VERSION=$CUSTOMVERSION
+LOCALVERSION_STRING="-$CUSTOMVERSION"
+DEFCONFIG_STRING=cyanogenmod_epicmtd_defconfig
+
+TOOLCHAIN=/mnt/data/linaro-android-eabi-1209/bin
+TOOLCHAIN_PREFIX=arm-eabi-
+
+PRJROOT=$PWD
+KERNEL_BUILD_DIR=$PRJROOT/kernel
+ZIP_BUILD_DIR=$PRJROOT/zip
+
+case "$1" in
+	clean)
+		pushd $KERNEL_BUILD_DIR
+		make V=1 ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX clean
+		popd
+		;;
+	mrproper)
+		pushd $KERNEL_BUILD_DIR
+		make V=1 ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX clean
+		make mrproper
+		popd
+		;;
+	distclean)
+		pushd $KERNEL_BUILD_DIR
+		make V=1 ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX clean
+		make distclean
+		popd
+		;;
+	build)
+		pushd $KERNEL_BUILD_DIR
+		START_TIME=`date +%s`
+		make ARCH=arm $DEFCONFIG_STRING
+		make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX LOCALVERSION=$LOCALVERSION_STRING
+		END_TIME=`date +%s`
+		let "ELAPSED_TIME=$END_TIME-$START_TIME"
+		echo "Total compile time is $ELAPSED_TIME seconds"
+		cp arch/arm/boot/zImage $ZIP_BUILD_DIR
+		popd
+		;;
+	zip)
+		pushd $ZIP_BUILD_DIR
+		cp $KERNEL_BUILD_DIR/arch/arm/boot/zImage $ZIP_BUILD_DIR
+		rm -rf system
+		mkdir -p system/lib/modules
+		find $KERNEL_BUILD_DIR -name '*.ko' -exec cp '{}' system/lib/modules/ \; -exec echo "Found module: '{}'" \;
+		$TOOLCHAIN/$TOOLCHAIN_PREFIX'strip' --strip-debug -v system/lib/modules/*
+		rm $PRJROOT/$CUSTOMVERSION.zip
+		zip -r $PRJROOT/$CUSTOMVERSION.zip ./*
+		popd
+		;;
+	*)
+		echo "usage: [build|zip|clean|mrproper|distclean]"
+		;;
+esac
